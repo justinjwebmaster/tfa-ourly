c553338af769173f582f18d4f213451c16c71f8a
jwebmaster <justin@jwebmaster.be>
2023-06-02 18:18:03 +0200

    mise de l'animation gsap dasn une fonction stickyDev()

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,99 +7,77 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 
-// const devSections = gsap.utils.toArray(".section--dev");
+// Animation section--dev
+function stickyDev(){
+  gsap.to(".section--dev1", {
+    scale: 1,
 
-// var i = 0;
-// var j = 1;
-// var topSpace;
-// var endTopSpace;
+    scrollTrigger: {
+      trigger: ".section--dev1",
+      start: "top bottom",
+    }});
 
-// devSections.forEach(function(section) {
+  gsap.to(".section--dev1", {
+    position: "sticky",
+    top: "140px",
 
-//   topSpace = 200 + i + "px";
-//   endTopSpace = "bottom " + 200 + i + "px";
+    scrollTrigger: {
+      trigger: ".section--dev1",
+      start: "top 300px",
+    }});
 
-//   gsap.to(section,
-//     {position: "sticky", top: topSpace, scale: .9},
-//     {scrollTrigger: {
-//       trigger: section,
-//       start: "top 200px",
-//       end: endTopSpace,
-//       scrub: true,
-//       }
-//     });
+  gsap.to(".section--dev1", {
+    scale: .9,
 
-//   i = i + 40;
-    
-// });
-gsap.to(".section--dev1", {
-  scale: 1,
+    scrollTrigger: {
+      trigger: ".section--dev2",
+      start: "top 340px",
+      end: "top 240px",
+      scrub: true,
+    }});
 
-  scrollTrigger: {
-    trigger: ".section--dev1",
-    start: "top bottom",
-  }});
+  gsap.to(".section--dev2", {
+    position: "sticky",
+    top: "180px",
+    scale: 1,
 
-gsap.to(".section--dev1", {
-  position: "sticky",
-  top: "140px",
-  
-  scrollTrigger: {
-    trigger: ".section--dev1",
-    start: "top 300px",
-  }});
+    scrollTrigger: {
+      trigger: ".section--dev2",
+      start: "top 340px",
+    }});
 
-gsap.to(".section--dev1", {
-  scale: .9,
+  gsap.fromTo(".section--dev1", {scale: .9},{
+    scale: .8,
 
-  scrollTrigger: {
-    trigger: ".section--dev2",
-    start: "top 340px",
-    end: "top 240px",
-    scrub: true,
-  }});
+    scrollTrigger: {
+      trigger: ".section--dev3",
+      start: "top 380px",
+      end: "top 280px",
+      scrub: true,
+    }});
 
-gsap.to(".section--dev2", {
-  position: "sticky",
-  top: "180px",
-  scale: 1,
-  
-  scrollTrigger: {
-    trigger: ".section--dev2",
-    start: "top 340px",
-  }});
+  gsap.to(".section--dev2", {
+    scale: .9,
 
-gsap.fromTo(".section--dev1", {scale: .9},{
-  scale: .8,
+    scrollTrigger: {
+      trigger: ".section--dev3",
+      start: "top 380px",
+      end: "top 280px",
+      scrub: true,
+    }});
 
-  scrollTrigger: {
-    trigger: ".section--dev3",
-    start: "top 380px",
-    end: "top 280px",
-    scrub: true,
-  }});
+  gsap.to(".section--dev3", {
+    position: "sticky",
+    top: "220px",
+    scale: 1,
 
-gsap.to(".section--dev2", {
-  scale: .9,
+    scrollTrigger: {
+      trigger: ".section--dev3",
+      start: "top 380px",
+    }});
 
-  scrollTrigger: {
-    trigger: ".section--dev3",
-    start: "top 380px",
-    end: "top 280px",
-    scrub: true,
-  }});
-
-gsap.to(".section--dev3", {
-  position: "sticky",
-  top: "220px",
-  scale: 1,
-  
-  scrollTrigger: {
-    trigger: ".section--dev3",
-    start: "top 380px",
-  }});
-
-
+}
+stickyDev();
 
   // Sticky nav
 
@@ -107,7 +85,7 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
 
   var currentScrollpos = window.pageYOffset;
-  if(prevScrollpos > currentScrollpos) {
+  if (prevScrollpos > currentScrollpos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-120px";
